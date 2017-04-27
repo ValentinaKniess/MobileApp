894275280aaaf94738184cc4298e8e9fba13eb89
ValentinaKniess <valentinakniess@gmail.com>
2017-04-27 01:19:42 -0400

    SocialShare experiment

--- js/Assignment7.js
+++ js/Assignment7.js
@@ -57,11 +57,11 @@ function socialsharingDemo() {
   window.plugins.socialsharing.available(function(isAvailable) {
     if (isAvailable) {
       // use a local image from inside the www folder:
-//      window.plugins.socialsharing.share('Some text', 'Some subject', null, 'http://www.nu.nl');
-//      window.plugins.socialsharing.share('Some text');
+     window.plugins.socialsharing.share('Some text', 'Some subject', null, 'http://www.nu.nl');
+     window.plugins.socialsharing.share('Some text');
 
-//      window.plugins.socialsharing.share('test', null, 'data:image/png;base64,R0lGODlhDAAMALMBAP8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAUKAAEALAAAAAAMAAwAQAQZMMhJK7iY4p3nlZ8XgmNlnibXdVqolmhcRQA7', null, function(e){alert("success: " + e)}, function(e){alert("error: " + e)});
-      window.plugins.socialsharing.share('My text', 'My subject', 'https://www.google.nl/images/srpr/logo11w.png', null, function(){alert("ok")}, function(e){alert("error: " + e)});
+     window.plugins.socialsharing.share('test', null, 'data:image/png;base64,R0lGODlhDAAMALMBAP8AAP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAUKAAEALAAAAAAMAAwAQAQZMMhJK7iY4p3nlZ8XgmNlnibXdVqolmhcRQA7', null, function(e){alert("success: " + e)}, function(e){alert("error: " + e)});
+      window.plugins.socialsharing.share('Look at this Google logo', 'Goolgle Logo', 'https://www.google.nl/images/srpr/logo11w.png', null, function(){alert("ok")}, function(e){alert("error: " + e)});
       // alternative usage:
 
       // 1) a local image from anywhere else (if permitted):
